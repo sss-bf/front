397a273a3f884a5f9372a060b4777a1b0ba1c12b
“sungjunkim” <kis07150@naver.com>
2025-03-01 13:14:48 +0900

    S3 upload test

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TestComponent from "./components/TestComponent"; // POST 요청을 실행할 컴포넌트
 import App from "./App";
-import S3Upload from "./components/S3Upload";
+import S3Upload from "./components/S3Upload"
 import SSS from "./components/sss";
 
 
@@ -11,7 +11,7 @@ const AppRouter = () => {
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/test" element={<TestComponent />} />
+        <Route path="/test" element={<S3Upload />} />
         {/* <Route path="/upload" element={<S3Upload />} /> */}
         {/* <Route path="/sss" element={<SSS />} /> */}
       </Routes>
